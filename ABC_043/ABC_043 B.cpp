be8f53fdea9e76fa4f3b705a6074d7cf46e9220f
Shunsuke Shimura <cibicibi2017@yahoo.co.jp>
2020-04-29 11:41:47 +0900

    提出：ABC_043 B

---
+++ ABC_043/ABC_043 B.cpp
@@ -0,0 +1,29 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii =  pair<int, int>;
+using pll =  pair<long long, long long>;
+constexpr char ln =  '\n';
+constexpr long long MOD = 1000000007LL;
+constexpr long long INF = 1000000000LL;
+#define all(x) (x).begin(),(x).end()
+#define rep(i,n) for(int i=0;i<(n);i++)
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+
+int main() {
+    string s, s_;
+    cin >> s;
+
+    rep(i, s.size()){
+        char c = s[i];
+        if(c == '0' || c == '1')
+            s_ += c;
+        else if (s_.size())
+            s_.erase(s_.size() - 1);
+    }
+    cout << s_ << ln;
+}
+    
