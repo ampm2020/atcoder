63184b6c633177daa23a39a197e523494de48012
Shunsuke Shimura <daigoro-chucchu2004@ezweb.ne.jp>
2020-05-29 23:16:18 +0900

    提出 5/29

--- ABC_152/E Flatten.cpp
+++ ABC_152/E Flatten.cpp
@@ -6,68 +6,11 @@ using pii =  pair<int, int>;
 using pll =  pair<long long, long long>;
 constexpr char ln =  '\n';
 constexpr long long MOD = 1000000007LL;
-constexpr long long INF = 1000000000LL;
+constexpr long long INF = 1000000009LL;
 #define all(x) (x).begin(),(x).end()
 #define rep(i,n) for(int i=0;i<(n);i++)
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
-
-template<int MOD> struct Fp {
-    long long val;
-    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
-        if (val < 0) val += MOD;
-    }
-    constexpr int getmod() { return MOD; }
-    constexpr Fp operator - () const noexcept {
-        return val ? MOD - val : 0;
-    }
-    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
-    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
-    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
-    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
-    constexpr Fp& operator += (const Fp& r) noexcept {
-        val += r.val;
-        if (val >= MOD) val -= MOD;
-        return *this;
-    }
-    constexpr Fp& operator -= (const Fp& r) noexcept {
-        val -= r.val;
-        if (val < 0) val += MOD;
-        return *this;
-    }
-    constexpr Fp& operator *= (const Fp& r) noexcept {
-        val = val * r.val % MOD;
-        return *this;
-    }
-    constexpr Fp& operator /= (const Fp& r) noexcept {
-        long long a = r.val, b = MOD, u = 1, v = 0;
-        while (b) {
-            long long t = a / b;
-            a -= t * b; swap(a, b);
-            u -= t * v; swap(u, v);
-        }
-        val = val * u % MOD;
-        if (val < 0) val += MOD;
-        return *this;
-    }
-    constexpr bool operator == (const Fp& r) const noexcept {
-        return this->val == r.val;
-    }
-    constexpr bool operator != (const Fp& r) const noexcept {
-        return this->val != r.val;
-    }
-    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
-        return os << x.val;
-    }
-    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
-        if (n == 0) return 1;
-        auto t = modpow(a, n / 2);
-        t = t * t;
-        if (n & 1) t = t * a;
-        return t;
-    }
-};
-using mint = Fp<1000000007>;
 
 vector<pair<long long, long long> > prime_factorize(long long n) {
     vector<pair<long long, long long> > res;
@@ -81,26 +24,48 @@ vector<pair<long long, long long> > prime_factorize(long long n) {
     return res;
 }
 
-int main() {
-    int n; cin >> n;
-    vector<ll> a(n);
-    vector<ll> num(1100000, 0); //素因数が何個あるか
+ll rui(ll a,ll b){
+     ll ans=1;
+     while(b>0){
+        if(b&1) ans=ans*a%MOD;
+        a=a*a%MOD;
+        b/=2;
+    }
+     return ans;
+}
+
+long long modinv(long long a, long long m) {
+    long long b = m, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+
+int main(){
+    int n; cin >> n; 
+    vector<ll> A(n), num(1100000, 0);
     rep(i, n){
-        cin >> a[i];
-        auto pf = prime_factorize(a[i]); //a[i]を素因数分解
-        for(auto p: pf) chmax(num[p.first], p.second);//各素因数の最大数をとる
+        cin >> A[i];
+        auto pf = prime_factorize(A[i]);
+        for(auto p: pf) chmax(num[p.first], p.second);  
     }
-
-    mint LCM = 1;
+    
+    ll LCM = 1;
     for(int v = 2; v < 1100000; v++){
-        LCM *= modpow(mint(v), num[v]);
+        LCM = LCM * rui(v, num[v]) % MOD;
     }
 
-    mint res = 0;
-    for(auto m: a){
-        res += LCM / m; 
+    ll res = 0;
+    for(auto a: A){
+        ll inv = modinv(a, MOD);
+        ll tmp = LCM * inv % MOD;
+        res = (res + tmp) % MOD;
     }
     cout << res << ln;
 }
-
 
